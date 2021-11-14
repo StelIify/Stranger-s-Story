4a700c864ef5fb44c8cdbf1e405f58f82249d1c2
Oleksandr Zorenko <34519030+StelIify@users.noreply.github.com>
2021-11-14 19:29:02 +0100

    character animation, basoic enemy ai

---
+++ Stranger's story/Assets/Scripts/Enemy/EnemyAttack.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    [SerializeField] int damage = 20;
+    PlayerStats player;
+    private void Awake()
+    {
+        player = FindObjectOfType<PlayerStats>();
+    }
+    
+    
+    public void AttackHitEvent()
+    {
+        if(player != null)
+        {
+            player.TakeDamage(damage);
+            Debug.Log("Bang Bang");
+        }
+    }
+
+    
+}
--- Stranger's story/Assets/Scripts/PlayerController.cs
+++ Stranger's story/Assets/Scripts/PlayerController.cs
@@ -10,9 +10,10 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float jumpForce = 2f;
 
     [SerializeField] private Transform camera;
+    [SerializeField] private Animator animator;
     private CharacterController characterController;
     private Vector3 moveDirection;
-    private bool groundedPlayer;
+    private bool playerIsGrounded;
     private void Awake()
     {
         characterController = GetComponent<CharacterController>();
@@ -20,18 +21,22 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        groundedPlayer = characterController.isGrounded;
-
-        if (groundedPlayer && moveDirection.y < 0)
-        {
-            moveDirection.y = 0f;
-        }
+        playerIsGrounded = characterController.isGrounded;
         
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
         var inputDirection = new Vector3(horizontal, 0, vertical).normalized;
+        
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
 
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        
+        animator.SetBool("IsMoving", isWalking);
+
+        
         if (inputDirection.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
@@ -40,17 +45,26 @@ public class PlayerController : MonoBehaviour
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
 
-            characterController.Move(moveDir * (moveSpeed * Time.deltaTime));
+            characterController.Move(moveDir.normalized * (moveSpeed * Time.deltaTime));
         }
 
-        if (Input.GetKey(KeyCode.Space) && groundedPlayer)
+        if (Input.GetKey(KeyCode.Space) && playerIsGrounded)
         {
-            moveDirection.y = Mathf.Sqrt(jumpForce * -3.0f * gravity);
+            moveDirection.y = jumpForce;
+            animator.SetBool("IsJumping", true);
         }
+        else if (characterController.isGrounded)
+        {
+            moveDirection.y = 0f;
+            animator.SetBool("IsJumping", false);
+        }
+            
+        
+        else
+            moveDirection.y -= gravity * Time.deltaTime;
 
-        moveDirection.y += gravity * Time.deltaTime;
-        characterController.Move(moveDirection * Time.deltaTime);
-
+        characterController.Move(moveDirection);
+        
     }
     
 }
